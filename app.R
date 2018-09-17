04115bacc02a5ece8cb79e9b8095de33b98e5527
AlexSandoval <a.sandoval0014>
2018-09-16 22:37:07 -0400

    Add Intro Comments
    Added comments to where the data can be found and what was removed

--- app.R
+++ app.R
@@ -7,6 +7,8 @@ library(plotly)
 library(shinythemes)
 library(stringr)
 
-#upload Philly shooting victim data from Opendataphilly
+# Upload Philadelphia property assessment data from Opendataphilly
+# Many fields were removed to decrease data upload
+# Data can be found here: https://www.phila.gov/property/data/
 property <- read.csv ("projectdata.csv")
 
